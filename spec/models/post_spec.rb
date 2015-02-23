3b5429998ea95a3537b95295a744b1487bed7a3c
Harry Chen <harrychenca@gmail.com>
2015-02-24 01:10:32 +0800

    Added comments to rspec model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,16 +3,22 @@ require 'rails_helper'
 RSpec.describe Post, :type => :model do
   context "trying to make 2 posts within 1 minute" do
     it "should raise error" do
+      # Create a user
       user = User.create!(:email => "foo@bar.com", :password => "foobarfoo")
+      
+      # Create one post
       user.posts.create!(:title => "foobar", :url => "http://www.foobar.com")
+      
+      # Create another post immediately within 1 minute, expect error
       expect{
         user.posts.create!(:title => "foobar", :url => "http://www.foobar.com")
-        }.to raise_error(ActiveRecord::RecordInvalid)
+      }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 
   context "trying to make more than 20 posts per user" do
     it "should raise error" do
+      # Create a user
       user = User.create!(:email => "foo@bar.com", :password => "foobarfoo")
 
       # Make 20 posts
@@ -20,10 +26,10 @@ RSpec.describe Post, :type => :model do
         user.posts.create!(:title => "foobar", :url => "http://www.foobar.com", :created_at => Time.now - i * 2.minutes)
       end
 
-      # Make 21th post
+      # Make 21th post, expect error
       expect{
         user.posts.create!(:title => "foobar", :url => "http://www.foobar.com")
-        }.to raise_error(ActiveRecord::RecordInvalid)
+      }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
